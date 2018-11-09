ae18e9ef9c65ead94ffd2b01978c9c2087b187df
elifsezer <ellfsezer60@gmail.com>
2018-11-09 16:05:39 +0300

    Testler bolumlere ayrıldı.

---
+++ app/src/main/java/com/example/elif/quizzapp/icerikActivity.kt
@@ -0,0 +1,12 @@
+package com.example.elif.quizzapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class icerikActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_icerik)
+    }
+}
